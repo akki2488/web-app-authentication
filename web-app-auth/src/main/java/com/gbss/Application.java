ffa000c1b023a608415947db7ccd603b803519d2
akshay-misra-backend-products <akshaymishra2488@gmail.com>
2020-07-14 02:21:39 +0530

    web app auth microservice branch first commit

--- web-app-auth/src/main/java/com/gbss/Application.java
+++ web-app-auth/src/main/java/com/gbss/Application.java
@@ -2,6 +2,7 @@ package com.gbss;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.data.mongodb.config.EnableMongoAuditing;
 
 /**
@@ -9,6 +10,7 @@ import org.springframework.data.mongodb.config.EnableMongoAuditing;
  */
 @SpringBootApplication
 @EnableMongoAuditing
+@EnableDiscoveryClient
 public class Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
